0f02a76d6f49827f5c9a8afee4e2825815681b53
seodongsu <xl445566@naver.com>
2022-02-03 20:41:35 +0900

    chore: Init

---
+++ contorollers/authController.js
@@ -0,0 +1,8 @@
+const authService = require("../services/authService");
+const roomService = require("../services/roomService");
+const communityService = require("../services/communityService");
+const constants = require("../utils/constants");
+
+exports.login = async (req, res, next) => { };
+
+exports.logout = async (req, res, next) => { };
